3ca83b1a09cde9882347e700b8fe36f4e8ecf0f5
anirudh mathur <anirudhmathur678@gmail.com>
2024-01-30 01:06:47 +0530

    c

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 export default function Home() {
+ const state=false
   return (
       <>
-      <h1> Hello to Personal Project</h1>
+      <div className="flex-col">
+      <h1 className="text-3xl font-bold text-indigo-500"> Hello to Personal Project</h1>
+      <Button className={cn("bg-indigo-500",state && "bg-red-500")} >Click Me !</Button>
+      </div>
       </>
+
   );
 }
