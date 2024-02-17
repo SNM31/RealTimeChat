79391df9062f14176aced606549d9b578b7d2d7a
anirudh mathur <anirudhmathur678@gmail.com>
2024-02-17 22:46:08 +0530

    ErrorRemoved

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,4 +1,19 @@
+import { InitialProfile } from "@/lib/intial-profile";
+import {db} from "@/lib/db"
 const SetupPage = async() => {
+    const profile= await InitialProfile()
+    if(typeof profile === 'object' && profile !== null && 'id' in profile)
+    {
+    const server= await db.server.findFirst({
+        where:{
+            members:{
+                some:{
+                    profileId:profile.id
+                }
+            }
+        }
+    });
+}
     return <div>Create A Server</div>;
 }
  
