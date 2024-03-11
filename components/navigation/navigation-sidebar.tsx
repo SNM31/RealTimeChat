942cb090cfea75767ecce37026ef748202bb40b4
anirudh mathur <anirudhmathur678@gmail.com>
2024-03-11 20:59:10 +0530

    2:47:17

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -3,9 +3,12 @@ import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 import { NavigationAction } from "./navigation-action";
 import { Separator } from "../ui/separator";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import NavigationItem from "./navigation-item";
+
 
 export const NavigationSideBar=async ()=>{
-    try{
+    
        const profile= await currentProfile()
        if(!profile) return redirect('/')
        
@@ -19,18 +22,24 @@ export const NavigationSideBar=async ()=>{
         }
        })
 
-    }
-    catch(error)
-    {
-        console.log(error);
-        
-    }
     return (
         <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
            <NavigationAction/>
            <Separator
            className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" 
            />
+            <ScrollArea className='flex-1 w-full'>
+                 {servers.map((server)=>(
+                     <div key={server.id}>
+                       <NavigationItem
+                       id={server.id}
+                       imageUrl={server.imageUrl}
+                       name={server.name}
+                       />
+                     </div>
+                 ))}
+            </ScrollArea>
         </div>
+       
     )
 }
